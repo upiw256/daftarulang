cefbe800766bb6af870f7fe92895c27b9ff15677
upiw256 <bilqimlb@gmail.com>
2021-06-18 22:37:10 +0700

    tampilan pertama

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Informasi Cek hasil PPDB di SMAN 1 Margaasih tahun 2021">
+    <meta name="author" content="SMAN 1 MARGAASIH - @sman1margaasih">
+    <title>PPDB SMAN 1 MARGAASIH</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <style>
+    html,
+    body {
+        height: 100%;
+        overflow-x: hidden;
+    }
+    </style>
+</head>
+<body class="bg-Light" oncontextmenu="return false">
+<div class="row align-items-center">
+    <div class="container">
+        <div class="row justify-content-center">
+        <img class="mb-3 img-responsive" src="img/spanduk.jpg" alt="sepanduk ppdb" style="width:90%;height:90%;">
+            
+
+            <div class="col-7 bg-success rounded">
+            <h2 class="text-light text-center mt-2">CEK HASIL</h2>
+            <form class="mt-3 mb-3" action="#" method="post">
+            <div class="row">
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="No Pendaftaran" aria-label="No Pendaftaran">
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col">
+                    <input type="submit" class=" form-control btn btn-primary" placeholder="Last name" aria-label="Last name">
+                </div>
+            </div>
+            </form>
+            <p class="fw-light text-light">- ini khusus untuk yang mendaftar ke SMAN 1 Margaasih</p>
+            <p class="fw-light text-light">- Jika tidak mengetahui nomor pendaftaran silahkan buka <span class="badge bg-light"><a href="https://pendaftar.ppdb.disdik.jabarprov.go.id/" target="_blank" class="text-decoration-none">pendaftar.ppdb.jabarprov.go.id</a></span> lalu login</p>
+            <p class="fw-light text-light">- Atau anda bisa membuka situs <span class="badge bg-light"><a href="https://ppdb.disdik.jabarprov.go.id/data-pendaftar/bandung/sma/negeri/608f7e1f19a57c0012556b6b" target="_blank" class="text-decoration-none">ppdb.jabarprov.go.id</a></span></p>
+            </div>
+            <div class="row mt-2">
+            <p class="text-center">© 2021 - SMAN 1 MARGAASIH</p>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+</script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</body>
+</html>
